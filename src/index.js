b7996a6989c2913911f5cc157a54a7b600b3fe1b
Junfel Velasco <vjunfel@gmail.com>
2023-01-08 11:03:31 +0800

    insert add and delete functionality with firestore database.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import {initializeApp} from 'firebase/app'
 import {
-  getFirestore, collection, getDocs
+  getFirestore, collection, getDocs,
+  addDoc, deleteDoc, doc
   } from 'firebase/firestore'
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyBSklDbe-Ew-escoTY3s9jlkvggvGLOdms",
-    authDomain: "fir-9-demo-c8d59.firebaseapp.com",
-    projectId: "fir-9-demo-c8d59",
-    storageBucket: "fir-9-demo-c8d59.appspot.com",
-    messagingSenderId: "46930440223",
-    appId: "1:46930440223:web:d8e5947d849da9ec05252a"
+const firebaseConfig = {
+  apiKey: "AIzaSyBSklDbe-Ew-escoTY3s9jlkvggvGLOdms",
+  authDomain: "fir-9-demo-c8d59.firebaseapp.com",
+  projectId: "fir-9-demo-c8d59",
+  storageBucket: "fir-9-demo-c8d59.appspot.com",
+  messagingSenderId: "46930440223",
+  appId: "1:46930440223:web:d8e5947d849da9ec05252a"
   };
 
 // init firebase app
@@ -33,3 +34,31 @@ getDocs(colRef)
   .catch(err => {
     console.log(err.message)
   })
+
+// adding documents
+const addBookForm = document.querySelector('.add')
+addBookForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  addDoc(colRef, {
+    title: addBookForm.title.value,
+    author: addBookForm.author.value,
+  })
+    .then(() => {
+      addBookForm.reset()
+    })
+})
+
+
+// deleting documents
+const deleteBookForm = document.querySelector('.delete')
+deleteBookForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const docRef = doc(db, 'books', deleteBookForm.id.value)
+
+  deleteDoc(docRef)
+  .then(() => {
+    deleteBookForm.reset()
+  })
+})
